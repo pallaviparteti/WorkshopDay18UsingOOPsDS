e962e4f6bd56a2a91fb37691156cca65692c34f8
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-03-05 22:58:39 +0530

    Added CreateEmptyBoardOne.java file

---
+++ CreateEmptyBoardOne.java
@@ -0,0 +1,28 @@
+package com.tictactoegame;
+
+public class CreateEmptyBoardOne
+{
+	//create a board of char array of size 10
+	static char[] board = new char[10];
+	static char userLetter;
+	static char computerLetter;
+
+	public static void main(String[] args)
+	{
+		createEmptyBoard();
+	}
+	/**
+	 * UC1
+	 * Method Name: createEmptyBoard
+	 * Description: Static method to create empty board
+	 * Algorithm: It takes board array indexes from 1 to 9 using for loop and
+	 * assign empty space to each element in the board.
+	 */
+	private static void createEmptyBoard()
+	{
+		for (int index = 1; index < board.length; index++)
+		{
+			board[index] = ' ';
+		}
+	}
+}
